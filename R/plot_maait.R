d8b0cd79347d3c90a1c33c41bae41e063ee8ce09
ZebinWang123 <14307110178@fudan.edu.cn>
2019-10-16 16:09:04 -0400

    Package is Functional

--- R/plot_maait.R
+++ R/plot_maait.R
@@ -1,22 +1,24 @@
 #' Plot Candy Popularity
 #'
-#' @description This function produces a bar plot of the most popular Halloween candies
+#' @description This function produces a plot with smoothing curves for variables in the MAAIT research
 #'
-#' @param df a dataframe containing information on the candies
-#' @param n the number of top candies to plot.  Default is set at 10
-#' @param candy_name_col the name of the column in \code{df} that specifies the names of the candies
-#' @param rank_col the name of the column in \code{df} that specifies how to rank the candies
+#' @param df a dataframe containing the MAAIT data
+#' @param x_axis_var the name of the column in \code{df} that specifies the name of the x-axis variable
+#' @param y_axis_var the name of the column in \code{df} that specifies the name of the y-axis variable
 #'
 #' @return a ggplot object
 #'
 #' @examples
-#' plot_maait(maait_data, candy_name_col = "competitorname", rank_col = "winpercent")
+#' plot_maait(maait_data, x_axis_var = "dmouseblog2", y_axis_var = "sxsgeneral")
 #' @import dplyr
 #' @import ggplot2
 #' @export
 
-plot_maait = function(df, n = 10, candy_name_col, rank_col){
-  p = 1
+plot_maait = function(df, x_axis_var, y_axis_var){
+  p <- ggplot(data = df, aes(x_axis_var, y_axis_var)) +
+    geom_point() +
+    geom_smooth(method = "gam", formula = y ~ s(x)) +
+    theme_bw()
 
   return(p)
 }
